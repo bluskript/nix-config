92c492969e1877e7606465d58d378316605f6024
Blusk <bluskript@gmail.com>
2023-06-06 06:34:29 -0400

    more funnies

--- hosts/common/profiles/desktop.nix
+++ hosts/common/profiles/desktop.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./base_cli.nix
     ../nix.nix
@@ -15,4 +15,22 @@
 
   dns.encryption.enable = true;
   networking.networkmanager.enable = true;
+
+  security.polkit.enable = true;
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 }
