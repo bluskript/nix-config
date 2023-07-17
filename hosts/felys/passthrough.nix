d5039279105301cf302ccd29b21662edc22cd077
Blusk <bluskript@gmail.com>
2023-07-16 20:17:36 -0500

    fix vm scripts

--- hosts/felys/passthrough.nix
+++ hosts/felys/passthrough.nix
@@ -67,15 +67,15 @@
   #   -msg timestamp=on
   # '';
   startVM = pkgs.writeScriptBin "startVM" ''
-    systemctl set-property-runtime -- user.slice AllowedCPUs=0-23
-    systemctl set-property-runtime -- system.slice AllowedCPUs=0-23
-    systemctl set-property-runtime -- init.scope AllowedCPUs=0-23
+    systemctl set-property --runtime -- user.slice AllowedCPUs=0-23
+    systemctl set-property --runtime -- system.slice AllowedCPUs=0-23
+    systemctl set-property --runtime -- init.scope AllowedCPUs=0-23
     virsh start $1
   '';
   stopVM = pkgs.writeScriptBin "stopVM" ''
-    systemctl set-property-runtime -- user.slice AllowedCPUs=0-31
-    systemctl set-property-runtime -- system.slice AllowedCPUs=0-31
-    systemctl set-property-runtime -- init.scope AllowedCPUs=0-31
+    systemctl set-property --runtime -- user.slice AllowedCPUs=0-31
+    systemctl set-property --runtime -- system.slice AllowedCPUs=0-31
+    systemctl set-property --runtime -- init.scope AllowedCPUs=0-31
     virsh shutdown $1
   '';
 in {
