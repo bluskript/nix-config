bde176ea2af08350aa2d451ea28e1d91a1424336
Blusk <bluskript@gmail.com>
2023-09-13 23:12:44 -0700

    captive browser

--- hosts/common/profiles/desktop.nix
+++ hosts/common/profiles/desktop.nix
@@ -38,4 +38,10 @@
       };
     };
   };
+
+  programs.captive-browser = {
+    enable = true;
+    dhcp-dns = "nmcli --terse --get-values IP4.DNS device show";
+    interface = "wlp0s20f3";
+  };
 }
