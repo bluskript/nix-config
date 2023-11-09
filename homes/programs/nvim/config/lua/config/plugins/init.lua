4b3eca7ffd323431b25371db1b774a1f16cfd646
Blusk <bluskript@gmail.com>
2023-11-09 11:17:29 -0800

    enable harpoon

--- homes/programs/nvim/config/lua/config/plugins/init.lua
+++ homes/programs/nvim/config/lua/config/plugins/init.lua
@@ -170,23 +170,32 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"ThePrimeagen/harpoon",
-	-- 	keys = {
-	-- 		{
-	-- 			"<S-m>",
-	-- 			function()
-	-- 				require("harpoon.mark").add_file()
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"<C-m>",
-	-- 			function()
-	-- 				require("harpoon.ui").toggle_quick_menu()
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- },
+	{
+		"ThePrimeagen/harpoon",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function(_, opts)
+			local mark = require("harpoon.mark")
+			local ui = require("harpoon.ui")
+
+			vim.keymap.set("n", "<leader>m", mark.add_file)
+			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+			vim.keymap.set("n", "<leader>1", function()
+				ui.nav_file(1)
+			end)
+			vim.keymap.set("n", "<leader>2", function()
+				ui.nav_file(2)
+			end)
+			vim.keymap.set("n", "<leader>3", function()
+				ui.nav_file(3)
+			end)
+			vim.keymap.set("n", "<leader>4", function()
+				ui.nav_file(4)
+			end)
+
+			require("harpoon").setup(opts)
+		end,
+	},
 	{
 		"kylechui/nvim-surround",
 		event = "VeryLazy",
